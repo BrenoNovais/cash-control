575f0fa7c9bd67f5a6d66c982d7c3149d4d4292a
brenonovais <breno.novais@hotmail.com>
2024-02-05 15:19:05 -0300

    Criando um modal acessível

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,29 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import logo from '../../assets/logo-ignite.svg';
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import logo from '../../assets/logo-ignite.svg'
 
 export function Header() {
     return (
         <HeaderContainer>
             <HeaderContent>
-                <img src={logo} alt=" logo de dois triangulos translucidos sobrepostos na diagonal "/>
+                <img src={logo} alt=" logo de dois triangulos translucidos sobrepostos na diagonal " />
                 <h1>DT MONEY</h1>
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <Dialog.Portal>
+                        <Dialog.Overlay></Dialog.Overlay>
+                        <Dialog.Content>
+                            <Dialog.Title>Nova transação</Dialog.Title>
+
+                            <Dialog.Close />
+                        </Dialog.Content>
+                    </Dialog.Portal>
+                </Dialog.Root>
+                
             </HeaderContent>
         </HeaderContainer>
     )
